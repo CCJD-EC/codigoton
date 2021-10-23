a55e061ba6ddeed8cc8cb6d2517edb422cf8b0bd
Christian Carrera <jchristiandias@gmail.com>
2021-10-23 11:20:32 -0500

    Initial Commit

---
+++ src/main/java/co/codigoton/banco/cena_clientes_impl/App.java
@@ -0,0 +1,13 @@
+package co.codigoton.banco.cena_clientes_impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
